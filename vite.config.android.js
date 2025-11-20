d4e860f4e5ad57581fc61609d72ceda63a5d2e8b
encento <encento@gmail.com>
2025-11-20 15:56:00 +0300

    Update vite.config.android.js

--- vite.config.android.js
+++ vite.config.android.js
@@ -1,2 +1,11 @@
+// vite.config.android.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
+  base: './',              // ВАЖНО: относительные пути, а не /DzenGun-STE/
+  build: {
+    outDir: 'dist-android' // отдельная папка для андроид-сборки
+  }
+});
