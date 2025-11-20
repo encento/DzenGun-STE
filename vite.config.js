45984980a0e76c67b3b128a298c568c56e64cff1
encento <encento@gmail.com>
2025-11-20 15:46:09 +0300

    major changes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/DzenGun-STE/",
+  // было: base: "/DzenGun-STE/",
+  base: "./",           // <-- так будет работать и на GitHub Pages, и из файла
   plugins: [react()],
 });
-
